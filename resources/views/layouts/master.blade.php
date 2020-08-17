207455b09ae7b434acef4e6b616bc84da446cb71
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-08-17 09:30:55 -0500

    feat: show errors with laravel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,11 +5,23 @@
     <title>Learn Laravel</title>
 </head>
 <body>
+    
     @if (session()->has('error'))
         <div class="alert alert-danger">
             {{ session()->get('error') }}
         </div>
     @endif
+
+    @if (isset($errors) && $errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
     @yield('content')
 </body>
 </html>
