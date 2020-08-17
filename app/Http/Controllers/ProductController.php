58e8d2706d53b595adc699ceced844b88866af28
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-08-17 09:14:22 -0500

    feat: create new views for products(index,show,create) and modify the products controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('products.index')->with([
+            'products' => Product::all(),
+        ]);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store()
+    {
+         // $product = Product::create([
+        //     'title' => request()->title,
+        //     'description' => request()->description,
+        //     'price' => request()->price,
+        //     'stock' => request()->stock,
+        //     'status' => request()->status,
+        // ]);
+
+        $product = Product::create(request()->all());
+
+        return $product;
+    }
+
+    public function show($product)
+    {
+        $product = Product::findOrFail($product);
+
+        return view('products.show')->with([
+            'product' => $product,
+        ]);
+    }
+
+    public function edit($product)
+    {
+        return "Showing the form to edit the product with id {$product}";
+    }
+
+    public function update($product)
+    {
+        //
+    }
+
+    public function destroy($product)
+    {
+        //
+    }
 }
---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>{{ $product->title }} ({{ $product->id }})</h1>
+    <p>{{ $product->description }}</p>
+    <p>{{ $product->price }}</p>
+    <p>{{ $product->stock }}</p>
+    <p>{{ $product->status }}</p>
+@endsection
