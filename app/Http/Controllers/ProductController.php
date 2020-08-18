2731832264b20f3006d1b49fdf41550f3088edbc
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-08-17 20:46:35 -0500

    feat: add a middleware for products controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('products.index')->with([
