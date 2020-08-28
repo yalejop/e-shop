eb97c1cbbb3c6752beef44a42eebaa118b37547f
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-08-27 23:26:59 -0500

    feat: user anidate resource for add products to cart

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/', 'MainController@index')->name('main');
 
 Route::resource('products', 'ProductController');
 
+Route::resource('products.carts', 'ProductCartController')->only(['store', 'destroy']);
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
