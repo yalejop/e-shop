0c301e696e43bc84a8b0fa3b3cf23045f20a5860
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-09-04 10:23:11 -0500

    feat:use dependecy injection and show the products in cart

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::resource('products', 'ProductController');
 
 Route::resource('products.carts', 'ProductCartController')->only(['store', 'destroy']);
 
+Route::resource('carts', 'CartController')->only(['index']);
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
