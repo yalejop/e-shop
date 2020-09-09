45126dd4a139e8ea9a89764612a08a80eaf22e42
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-09-09 08:41:25 -0500

    feat:Using computed attributes for total calculate

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <h1>Order Details</h1>
 
+    <h4 class="text-center"><strong>Grand Total: </strong> {{ $cart->total }}</h4>
+
+
     <div class="table-responsive">
         <table class="table table-striped">
             <thead class="thead-light">
@@ -24,7 +27,7 @@
                     <td>{{ $product->pivot->quantity }}</td>
                     <td>
                         <strong>
-                            {{ $product->pivot->quantity * $product->price }}
+                            ${{ $product->total }}
                         </strong>
                     </td>
                 </tr>
